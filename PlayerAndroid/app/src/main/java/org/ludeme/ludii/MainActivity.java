aabd5c806e47a4d1303b2b2fe695eee73c47893b
Pierce Brooks <piercebrooks@rocketmail.com>
2026-03-02 23:28:22 -0500

    initial gradle android build system support...

---
+++ PlayerAndroid/app/src/main/java/org/ludeme/ludii/MainActivity.java
@@ -0,0 +1,18 @@
+package org.ludeme.ludii;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
